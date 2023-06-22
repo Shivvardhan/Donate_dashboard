c505d0d2ebbf86368bc5718583dc52d04612a22a
Shivvardhan <shivvardhan52@gmail.com>
2023-06-22 18:59:07 +0530

    new push

---
+++ logincss.php
@@ -0,0 +1,28 @@
+<style>
+.btn-color{
+  background-color: #0e1c36;
+  color: #fff;
+  
+}
+.btn-color:hover{
+  background-color:	#181818;
+  color: #fff;
+  
+}
+
+.profile-image-pic{
+  height: 200px;
+  width: 200px;
+  object-fit: cover;
+}
+
+
+
+.cardbody-color{
+  background-color: #ebf2fa;
+}
+
+a{
+  text-decoration: none;
+}
+</style>
