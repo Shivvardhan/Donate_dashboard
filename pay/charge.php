7ed3d45c86eb9e1c32c587e5aa8d7bac1ba3167d
Harshit Varshney <137395632+Harshitvigyos@users.noreply.github.com>
2023-07-03 20:30:47 +0530

    push

--- pay/charge.php
+++ pay/charge.php
@@ -2,20 +2,30 @@
 $api_key = 'rzp_test_nRUCpBxtiAUmco';
 $api_secret = '1xFc5ei1s3RMouHTYPUirakP';
 
-$paymentId = $_POST['razorpay_payment_id'];
+if (isset($_POST['razorpay_payment_id'])) {
+    $paymentId = $_POST['razorpay_payment_id'];
 
-$ch = curl_init("https://api.razorpay.com/v1/payments/$paymentId");
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_USERPWD, $api_key . ':' . $api_secret);
-$response = curl_exec($ch);
-$payment = json_decode($response);
+    $ch = curl_init("https://api.razorpay.com/v1/payments/$paymentId");
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_USERPWD, $api_key . ':' . $api_secret);
+    $response = curl_exec($ch);
+    $payment = json_decode($response);
 
-if ($payment->status === 'captured') {
-    // Payment successful, perform necessary actions
-    echo 'Payment successful! Order ID: ' . $payment->order_id;
+    echo 'Payment API Response: ';
+    // print_r($payment); // Debug output
+
+    if (isset($payment->status) && $payment->status === 'captured') {
+        // Payment successful, perform necessary actions
+        $order_id = isset($payment->order_id) ? $payment->order_id : 'unknown';
+        echo 'Payment successful! Order ID: ' . $order_id;
+    } else {
+        // Payment failed, handle accordingly
+        echo 'Payment failed!';
+    }
 } else {
-    // Payment failed, handle accordingly
-    echo 'Payment failed! Order ID: ' . $payment->order_id;
+    echo 'Invalid payment details.';
 }
+
+
 ?>
--- pay/index.php
+++ pay/index.php
@@ -9,111 +9,127 @@ $dbname = "sanju";
 $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+    die("Connection failed: " . $conn->connect_error);
 }
 
+if (isset($_POST['submitButton2'])) {
 
-if(isset($_POST['submitButton2'])){
+    function generateOrderID($length = 8)
+    {
+        $characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+        $orderId = '';
 
-function generateOrderID($length = 8) {
-  $characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
-  $orderId = '';
+        $characterCount = strlen($characters);
+        for ($i = 0; $i < $length; $i++) {
+            $randomCharacter = $characters[rand(0, $characterCount - 1)];
+            $orderId .= $randomCharacter;
+        }
 
-  $characterCount = strlen($characters);
-  for ($i = 0; $i < $length; $i++) {
-      $randomCharacter = $characters[rand(0, $characterCount - 1)];
-      $orderId .= $randomCharacter;
-  }
+        return $orderId;
+    }
 
-  return $orderId;
-}
+    $oid = generateOrderID();
 
-$oid = generateOrderID();
+    $amt = $_POST['amount'];
+    $fname = $_POST['fname'];
+    $dob = $_POST['dob'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $country = $_POST['country'];
+    $state = $_POST['state'];
+    $city = $_POST['city'];
+    $address = $_POST['address'];
+    $pincode = $_POST['pincode'];
+    $pan = $_POST['pan'];
+    $eligibilty = "eligible";
 
+    $sql = "INSERT INTO `donate`(`amount`, `full_name`, `dob`, `email`, `phone`, `country`, `state`, `city`, `address`, `pincode`, `pan`, `eligibilty`, `o_id`) VALUES ('$amt','$fname','$dob','$email','$phone','$country','$state','$city','$address','$pincode','$pan','$eligibilty','$oid')";
 
-$amt = $_POST['amount'];
-$fname = $_POST['fname'];
-$dob = $_POST['dob'];
-$email = $_POST['email'];
-$phone = $_POST['phone'];
-$country = $_POST['country'];
-$state = $_POST['state']; 
-$city = $_POST['city'];
-$address = $_POST['address'];
-$pincode = $_POST['pincode'];
-$pan = $_POST['pan'];
-// $eligibility = $_POST['eligibility'];
-$eligibility = $_POST['contactPermission'];
+    if ($conn->query($sql) === TRUE) {
+        $api_key = 'rzp_test_nRUCpBxtiAUmco';
+        $api_secret = '1xFc5ei1s3RMouHTYPUirakP';
 
-
+        $amount = 100*$amt ; // amount in the smallest currency unit (e.g., paise for INR)
     
+        $currency = 'INR';
+        $receipt = 'order_receipt';
 
-$sql = "INSERT INTO `donate`(`amount`, `full_name`, `dob`, `email`, `phone`, `country`, `state`, `city`, `address`, `pincode`, `pan`, `eligibilty`,`o_id`) VALUES ('$amt','$fname','$dob','$email','$phone','$country','$state','$city','$address','$pincode','$pan','$eligibility','$oid')";
+        // Step 1: Create an order
+        $data = array(
+            'amount' => $amount ,
+            'currency' => $currency,
+            'receipt' => $receipt
+        );
 
-if ($conn->query($sql) === TRUE) {
-    
+        $ch = curl_init('https://api.razorpay.com/v1/orders');
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_USERPWD, $api_key . ':' . $api_secret);
+        $response = curl_exec($ch);
+        $order = json_decode($response);
 
-?><?php
-$api_key = 'rzp_test_nRUCpBxtiAUmco';
-$api_secret = '1xFc5ei1s3RMouHTYPUirakP';
-
-$amount = 100; // amount in the smallest currency unit (e.g., paise for INR)
-$currency = 'INR';
-$receipt = 'order_receipt';
-
-// Step 1: Create an order
-$data = array(
-    'amount' => $amount,
-    'currency' => $currency,
-    'receipt' => $receipt
-);
-
-$ch = curl_init('https://api.razorpay.com/v1/orders');
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
-curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_USERPWD, $api_key . ':' . $api_secret);
-$response = curl_exec($ch);
-$order = json_decode($response);
-
-// Step 2: Generate the payment form
-$formHtml = '
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Razorpay Payment Form</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-</head>
-<body>
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-6">
-                <div class="card mt-5">
-                    <div class="card-body">
-                        <h4 class="card-title">Boonary Foundation Donation Payment</h4>
-                        <h5 class="card-title">Make a Payment</h5>
-                        <form action="charge.php" method="POST">
-                            <div class="form-group">
-                                <label for="amount">Amount (INR)</label>
-                                <input type="text" class="form-control" id="amount" name="amount" value="' . $order->amount . '" readonly>
+        // Step 2: Generate the payment form
+        $formHtml = '
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <title>Razorpay Payment Form</title>
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+            <style>
+    .razorpay-payment-button {
+        background-color: #528FF0;
+        color: #fff;
+        border: none;
+        padding: 10px 20px;
+        font-size: 16px;
+        border-radius: 4px;
+        cursor: pointer;
+    }
+</style>
+        </head>
+        <body>
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-md-6">
+                        <div class="card mt-5">
+                            <div class="card-body">
+                                <h4 class="card-title">Boonary Foundation Donation Payment</h4>
+                                <h5 class="card-title">Make a Payment</h5>
+                                <form action="charge.php" method="POST">
+                                <div class="form-group">
+                                    <label for="amount">Amount (INR)</label>
+                                    <input type="text" class="form-control" id="amount" name="amount" value="' . $amt . '" readonly>
+                                </div>
+                                <input type="hidden" name="order_id" value="' . $order->id . '">
+                                <script src="https://checkout.razorpay.com/v1/checkout.js"
+                                        data-key="' . $api_key . '"
+                                        data-amount="' . $order->amount . '"
+                                        data-currency="' . $currency . '"
+                                        data-order_id="' . $order->id . '"
+                                        data-buttontext="Pay with Razorpay"
+                                        data-name="Boonary Foundation Donation Payment"
+                                        data-description="Make a Payment"
+                                        data-prefill.name="' . $fname . '"
+                                        data-prefill.email="' . $email . '"
+                                        data-theme.color="#528FF0"
+                                        data-prefill.method="card">
+                                </script>
+                                
+                            </form>
+                            
                             </div>
-                           
-                            <button type="submit" class="btn btn-primary">Pay with Razorpay</button>
-                        </form>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </div>
-</body>
-</html>
-';
-echo $formHtml;
+        </body>
+        </html>
+        ';
+        echo $formHtml;
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+}
 ?>
 
-<?php 
-  } else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-  }
-  
-}?>
